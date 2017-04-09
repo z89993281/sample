1400bcc4d1ebd3240c00569cdf3d71f4dc57c372
iamnick <89993281@qq.com>
2017-04-09 05:02:45 +0000

    Test Push 13:02

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 -Nick 12:51</div>
+                <div class="title">Laravel 5 -Nick 13:02</div>
             </div>
         </div>
     </body>
