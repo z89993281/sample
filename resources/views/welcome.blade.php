0806093d81bdfa338c24252ec6b780fb33edd6e2
iamnick <89993281@qq.com>
2017-04-10 10:55:07 +0000

    TestTody0410

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 -Nick 13:02</div>
+                <div class="title">Laravel 5 -Nick 18:52</div>
             </div>
         </div>
     </body>
