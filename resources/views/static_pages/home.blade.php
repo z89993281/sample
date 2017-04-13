9646dd135e881fd2222804b7859ecfe5f303f351
iamnick <89993281@qq.com>
2017-04-13 16:22:53 +0000

    do 0:22

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
   <div class="jumbotron">
-    <h1>Hello Laravel</h1>
+    <h1>Hello Laravel - iamnick 0:20</h1>
     <p class="lead">
       你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的示例项目主页。
     </p>
